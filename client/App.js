6eec339dfc011c829345ed76ad3f8a744a55310d
Silky Ng <silky.ng@gmail.com>
2022-04-20 16:54:22 +0100

    feat(App.js): basic intro screen

--- client/App.js
+++ client/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import backgroundImageCover from './assets/login_background.jpg';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={backgroundImageCover} style={styles.image}/>
+        <Text style={styles.text}>Plant Spotters</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    color: 'black',
+    fontSize: 24,
+    lineHeight: 84
+  }
 });
