58f8c945d05f18f245b3e410a392b996b6407b7c
Silky Ng <silky.ng@gmail.com>
2022-04-20 17:07:49 +0100

    fix(App.js): installed @react-navigation/native-stack

--- client/App.js
+++ client/App.js
@@ -3,6 +3,8 @@ import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import backgroundImageCover from './assets/login_background.jpg';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,11 +12,17 @@ export default function App() {
 
   
   return (
-    <View style={styles.container}>
-      <Image source={backgroundImageCover} style={styles.image}/>
-        <Text style={styles.text}>Plant Spotters</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Image source={backgroundImageCover} style={styles.image}/>
+    //     <Text style={styles.text}>Plant Spotters</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
