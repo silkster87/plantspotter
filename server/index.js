611d905243cb9ab2933dd2d4c405cca8babf006a
Silky Ng <silky.ng@gmail.com>
2022-04-20 16:52:20 +0100

    refactor(index.js): minor adjustments

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const router = require('./router.js');
 require('dotenv').config();
+
 const PORT = process.env.PORT || 3030;
+
 app.use(cors());
-
+app.use(router);
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
