7353d8ca37b6ce47976652b830df6094405f818e
Silky Ng <silky.ng@gmail.com>
2022-04-20 18:12:22 +0100

    feat(LoginScreen): added firebase auth for email and password

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,14 +1,110 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import app from '../firebase'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(userCredentials => {
+      const user = userCredentials.user;
+      console.log(user.email);
+    })
+    .catch(err => alert(err.message));
+  }
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior='padding'
+    >
+      <View style={styles.inputContainer} >
+        <TextInput
+          placeholder="Email"
+          value= {email}
+          onChangeText= {text => setEmail(text)}
+          style={styles.input}
+        />
+          
+          <TextInput
+          placeholder="Password"
+          value= {password}
+          onChangeText= {password => setPassword(password)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.button}  
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}  
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40
+  },
+  button: {
+    backgroundColor: '#097F0C',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center'
+  }, 
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#097F0C',
+    borderWidth: 2
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16
+  },
+  buttonOutlineText: {
+    color: '#097F0C',
+    fontWeight: '700',
+    fontSize: 16
+  }
+
+})
