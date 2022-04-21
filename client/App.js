04bcc2137f77b438ca0b0c24fe7bf7114a0f9e37
Silky Ng <silky.ng@gmail.com>
2022-04-21 10:31:36 +0100

    refactor(App.js): removed unnecessary code

--- client/App.js
+++ client/App.js
@@ -1,8 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import backgroundImageCover from './assets/login_background.jpg';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 
@@ -13,11 +10,6 @@ export default function App() {
 
   
   return (
-    // <View style={styles.container}>
-    //   <Image source={backgroundImageCover} style={styles.image}/>
-    //     <Text style={styles.text}>Plant Spotters</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{ headerShown: false }}name="Login" component={LoginScreen} />
@@ -27,6 +19,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
