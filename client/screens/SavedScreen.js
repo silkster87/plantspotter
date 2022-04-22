9b95ca0ade3511d7d3649c36da633b0e9f6b12e8
Silky Ng <silky.ng@gmail.com>
2022-04-22 17:44:20 +0100

    feat(SavedScreen): added a modal that pops up to display more info about the selected plant

--- client/screens/SavedScreen.js
+++ client/screens/SavedScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Modal, Pressable, Image, TouchableOpacity, Linking } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { getAuth } from 'firebase/auth';
 const BASE_URL = 'https://0233-78-147-209-58.eu.ngrok.io';
 
 export default function SavedScreen() {
   const [plantsList, setPlantsList] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [plantItem, setPlantItem] = useState({});
 
   useEffect(() => {
     (async () => {
@@ -25,8 +27,8 @@ export default function SavedScreen() {
   }, []);
 
   function showPlantDetails(id) {
-    //console.log('Plant ID: ', id);
-
+    setPlantItem(plantsList.find(plant => plant._id == id));
+    setModalVisible(true);
   }
 
   const renderItem = ({ item }) => <TouchableOpacity 
@@ -40,13 +42,108 @@ export default function SavedScreen() {
 
   return (
     <View style = {styles.container}>
+
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible)
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Image
+              style={styles.modalImage}
+              source={{uri: plantItem.imageUrl}}
+            ></Image>
+            <Text style={styles.modalText} onPress={() => Linking.openURL(plantItem.plantInfoUrl)}>{plantItem.title}</Text>
+            <Text style={styles.description}>{plantItem.description}</Text>
+            <View style={styles.buttonsContainer}>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Ok</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Delete</Text>
+              </Pressable>
+            </View>
+            
+          </View>
+        </View>
+      </Modal>
+
       <FlatList data={plantsList} renderItem={renderItem} keyExtractor={item => item._id}/>
+
     </View>
     
   )
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    marginBottom: 15,
+    fontSize: 24,
+    color: '#2196F3',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
+  },
+  modalImage: {
+    width: 200,
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 20
+  },
+  description: {
+    fontSize: 15
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    margin: 10,
+    
+  },
+  buttonClose: {
+    width: 100,
+    backgroundColor: '#2196F3',
+    borderRadius: 5,
+    padding: 5,
+    justifyContent: 'center',
+    
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 20
+  },
   container: {
     flex: 1
   },
@@ -54,14 +151,20 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     resizeMode: 'cover',
-    marginBottom: 5
+    marginBottom: 5,
+    borderRadius: 5
   },
   button: {  
     flexDirection: 'row',
-    margin: 20
+    margin: 20,
+    
   },
   text: {
+    marginTop: 35,
     marginLeft: 15,
     fontSize: 20,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
   }
 })
