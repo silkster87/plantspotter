8b0bbff1f177e8114506a0ebbbd5d4ea3d8a9316
Silky Ng <silky.ng@gmail.com>
2022-04-25 11:16:43 +0100

    feat(Icon): added svg icons to login screen and header

--- client/App.js
+++ client/App.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, View } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import SvgPlantLogo from './assets/plantIcon.js';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,21 +14,37 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }}name="Login" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: false}}name="Login" component={LoginScreen} />
         <Stack.Screen 
           name="Plant Spotter" 
-          options={{
-            headerStyle: {
-              backgroundColor: '#097F0C',
-            },
-            headerTintColor: '#fff',
-            headterTitleStyle: {
-              fontWeight: 'bold',
-            }
-          }} 
+          options={options} 
           component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const options = {
+  headerStyle: {
+    backgroundColor: '#097F0C',
+  },
+  headerTintColor: '#fff',
+  headterTitleStyle: {
+    fontWeight: 'bold',
+  },
+  headerLeft: () => (
+    <View style={styles.logoView}>
+      <SvgPlantLogo />
+    </View>
+    
+  ),
+  
+};
+
+const styles = StyleSheet.create({
+  logoView: {
+    width: 48,
+    height: 48,
+  }
+})
 
