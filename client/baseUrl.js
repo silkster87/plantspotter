380c5fd3b7b44d7718d4195a82f31712abe4565d
Silky Ng <silky.ng@gmail.com>
2022-04-26 14:04:48 +0100

    refactor(baseUrl): added comments

--- client/baseUrl.js
+++ client/baseUrl.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://4992-78-147-211-58.eu.ngrok.io';
+//Temporary public ngrok server to service the localhost for testing.
+const BASE_URL = 'https://39e0-78-149-64-151.eu.ngrok.io';
 
 export default BASE_URL;
